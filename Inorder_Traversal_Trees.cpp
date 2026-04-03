bd5465901bccab39c6710d9e0af81fcc975e6021
Soumya Pathak <pathaksoumya2005@gmail.com>
2026-04-03 14:53:33 +0530

    Day 28- Tree (Inorder Traversal)

---
+++ Inorder_Traversal_Trees.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+
+    inorder(root->left);      // Left
+    cout << root->data << " "; // Root
+    inorder(root->right);     // Right
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inorder(root);  // Output: 4 2 5 1 3
+}
